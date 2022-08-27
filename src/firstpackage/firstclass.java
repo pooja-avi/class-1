b84df866a37a5ff6236435afe2c6ec499dc8f23a
Vijay <Vijay@DESKTOP-ECUR4I2>
2022-08-26 18:07:56 -0700

    my first program

---
+++ src/firstpackage/firstclass.java
@@ -0,0 +1,12 @@
+package firstpackage;
+
+public class firstclass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello first project");
+		// TODO Auto-generated method stub
+
+	}
+
+}
