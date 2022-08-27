2a4f18acf96d5d2de2a40b253e21e5aeb50a60ce
Vijay <Vijay@DESKTOP-ECUR4I2>
2022-08-26 18:48:08 -0700

    multipication

---
+++ src/firstpackage/mutlipication.java
@@ -0,0 +1,13 @@
+package firstpackage;
+
+public class mutlipication {
+	
+	public static void main (String[] args ) {
+		int a=50;
+		int b=30;
+		int c=a*b;
+		System.out.println(c);
+			
+				}
+
+}
