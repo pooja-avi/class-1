3230f0f8bb3900a386fddad5bf63af11a6ab6c86
Vijay <Vijay@DESKTOP-ECUR4I2>
2022-08-26 18:22:49 -0700

    datatypes commit

---
+++ src/firstpackage/Datatypes.java
@@ -0,0 +1,16 @@
+package firstpackage;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println("the value of c="+c);
+		System.out.println(c);
+	}
+
+}
